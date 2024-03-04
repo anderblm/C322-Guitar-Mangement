2bc4114f7ad29b96bc242a6d04dab87bec1463c6
HiC-BGOD <Blane3512@gmail.com>
2024-03-04 00:54:16 -0500

    Completed

--- src/main/java/edu/iu/anderblm/c322spring2024homework2/Controllers/InventoryController.java
+++ src/main/java/edu/iu/anderblm/c322spring2024homework2/Controllers/InventoryController.java
@@ -1,9 +1,6 @@
 package edu.iu.anderblm.c322spring2024homework2.Controllers;
 
-import edu.iu.anderblm.c322spring2024homework2.Model.Builder;
 import edu.iu.anderblm.c322spring2024homework2.Model.Guitar;
-import edu.iu.anderblm.c322spring2024homework2.Model.Type;
-import edu.iu.anderblm.c322spring2024homework2.Model.Wood;
 import edu.iu.anderblm.c322spring2024homework2.Repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +20,6 @@ public class InventoryController {
 
     @GetMapping("/search")
     public List<Guitar> searchGuitars(
-<<<<<<< HEAD
             @RequestParam String builder,
             @RequestParam String backWood,
             @RequestParam String model,
@@ -31,25 +27,6 @@ public class InventoryController {
             @RequestParam String topWood,
             @RequestParam String serialNumber,
             @RequestParam double price
-=======
-
-            @RequestParam Builder builder,
-            @RequestParam Wood backWood,
-            @RequestParam String model,
-            @RequestParam Type type,
-            @RequestParam Wood topWood,
-            @RequestParam String serialNumber,
-            @RequestParam double price
-
-            @RequestParam(required = false) String builder,
-            @RequestParam(required = false) String backWood,
-            @RequestParam(required = false) String model,
-            @RequestParam(required = false) String type,
-            @RequestParam(required = false) String topWood,
-            @RequestParam(required = false) String serialNumber,
-            @RequestParam(required = false) double price
->>>>>>> f1072738b3698feaa930078d5a8f7e5b1647b780
-
 
     ) {
         return inventoryRepository.search(new Guitar(serialNumber, price, builder, model, type, backWood, topWood));
