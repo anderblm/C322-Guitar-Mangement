4a358a46504c0be2ec5445a014806e2b8b85bafa
HiC-BGOD <Blane3512@gmail.com>
2024-03-04 00:57:26 -0500

    Completed

--- src/main/java/edu/iu/anderblm/c322spring2024homework2/Model/Guitar.java
+++ src/main/java/edu/iu/anderblm/c322spring2024homework2/Model/Guitar.java
@@ -4,29 +4,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 public class Guitar {
     private String serialNumber;
     private double price;
-    private Builder builder;
+    private String builder;
     private String model;
-    private Type type;
-    private Wood backWood;
-    private Wood topWood;
+    private String type;
+    private String backWood;
+    private String topWood;
 
     // Add a default constructor for Spring
-
-    public Guitar(String builder, String backWood) {
-
-
-    public Guitar(String builder, String backWood) {
-        // Implement constructor if needed
-
     public Guitar() {
-
-
     }
 
-    public Guitar(String serialNumber, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+    public Guitar(String serialNumber, double price, String builder, String model, String type, String backWood, String topWood) {
         this.serialNumber = serialNumber;
         this.price = price;
         this.builder = builder;
@@ -35,7 +27,6 @@ public class Guitar {
         this.backWood = backWood;
         this.topWood = topWood;
     }
-
 
     public String getSerialNumber() {
         return serialNumber;
@@ -49,7 +40,7 @@ public class Guitar {
         this.price = newPrice;
     }
 
-    public Builder getBuilder() {
+    public String getBuilder() {
         return builder;
     }
 
@@ -57,23 +48,21 @@ public class Guitar {
         return model;
     }
 
-    public Type getType() {
+    public String getType() {
         return type;
     }
 
-    public Wood getBackWood() {
+    public String getBackWood() {
         return backWood;
     }
 
-    public Wood getTopWood() {
+    public String getTopWood() {
         return topWood;
     }
 
-    // Getters and setters as needed
-
     public void writeToFile() {
         try (PrintWriter writer = new PrintWriter(new FileWriter("guitars_database.txt", true))) {
-            writer.println(serialNumber + "," + price + "," + builder.toString() + "," + model + "," + type.toString() + "," + backWood.toString() + "," + topWood.toString());
+            writer.println(serialNumber + "," + price + "," + builder + "," + model + "," + type + "," + backWood + "," + topWood);
             System.out.println("Guitar written to guitars_database.txt.");
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
